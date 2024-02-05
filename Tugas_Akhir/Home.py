f132edfd981a40e5a3d370abd407e16b0fa9ecd3
feriyantosrb <86914580+feriyantosrb@users.noreply.github.com>
2024-02-05 20:35:32 +0700

    Update Home.py

--- Tugas_Akhir/Home.py
+++ Tugas_Akhir/Home.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Optimasi CCS/CCUS HUB di Indonesia : Case Studi South Sumatra Area",
+    page_title="DecarbonSystem",
     page_icon="🟢",
     menu_items={"Get help":"mailto:feriyanto.idn@gmail.com"}
 )
 
-st.write("# Optimasi CCS/CCUS HUB di Indonesia : Case Studi South Sumatra Area 🟢")
+st.write("# DecarbonSystem: A Tool for Integrating Carbon Capture, Utilization, and Storage (CCUS) Value Chain in Indonesia : Case Study in South Sumatra Area")
 
 st.sidebar.header("")
 #st.sidebar.header(':blue[Pages]')
@@ -14,4 +14,4 @@ st.sidebar.header("")
 st.image(r"Tugas_Akhir/network.png", width=800)
 
 st.write('#')
-st.write("Feriyanto - Teknik Perminyakan ITB 2020")
+st.write("SiX - Team")
