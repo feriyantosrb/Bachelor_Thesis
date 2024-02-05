e02f227a8cdb6f1a50bddb72986d0d41dc7460fe
feriyantosrb <86914580+feriyantosrb@users.noreply.github.com>
2024-02-05 19:52:09 +0700

    Add files via upload

---
+++ Tugas_Akhir/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Optimasi CCS/CCUS HUB di Indonesia : Case Studi South Sumatra Area",
+    page_icon="🟢",
+    menu_items={"Get help":"mailto:feriyanto.idn@gmail.com"}
+)
+
+st.write("# Optimasi CCS/CCUS HUB di Indonesia : Case Studi South Sumatra Area 🟢")
+
+st.sidebar.header("")
+#st.sidebar.header(':blue[Pages]')
+    
+st.image(r"Tugas_Akhir/network.jpg", width=800)
+
+st.write('#')
+st.write("Feriyanto - Teknik Perminyakan ITB 2020")
